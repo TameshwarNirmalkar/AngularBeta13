0262111a7209f1b89e23fc420f6432c0f3a883ab
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2016-04-07 08:06:17 +0530

    missing lib added

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// Libraries
+import {bootstrap}        from 'angular2/platform/browser'
+import {APP_BASE_HREF, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import { bind } from "angular2/core";
+import { HTTP_PROVIDERS } from 'angular2/http';
+import 'rxjs/add/operator/map'; //this is fix for .map() method in http response.
+
+// Custom Components
+import {AppComponent}     from './app/app'
+
+bootstrap(AppComponent, [
+	ROUTER_PROVIDERS,
+	bind(LocationStrategy).toClass(HashLocationStrategy),
+	HTTP_PROVIDERS
+]);
