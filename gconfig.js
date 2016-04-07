0197bab7aee82f4bb9f6f1de20ccf16052ab4410
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2016-04-07 08:21:49 +0530

    config file add

---
+++ gconfig.js
@@ -0,0 +1,24 @@
+'use strict';
+var GulpConfig = (function () {
+    function gulpConfig() {
+
+        // Prequisites
+        this.source        =    './src/';
+        this.build         =    './dist/';
+        this.sourceApp     =    this.source + 'app/';
+
+        // Compile Paths
+        this.tsOutputPath   =   this.build  + '/js';
+        this.listFilesTS    =   this.source + '/**/*.ts';
+        this.listFilesSCSS  =   this.source + '/**/*+(scss|sass|css)';
+        this.listFilesHTML  =   this.source + '/**/*.html';
+        this.listFilesJS    =   this.source + '/**/*.js';
+        this.listFilesFonts =   this.source + 'fonts/**/*';
+
+        // Typings
+        this.typings       = './tools/typings/';
+        this.libraryTypeScriptDefinitions = './tools/typings/**/*.ts';
+    }
+    return gulpConfig;
+})();
+module.exports = GulpConfig;
